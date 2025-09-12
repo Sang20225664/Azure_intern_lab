f67a248682c5d8301c8f8d935352e074dc00e321
TSang <sang.ndt225664@sis.hust.edu.vn>
2025-09-12 23:29:22 +0700

    init

---
+++ variable.tf
@@ -0,0 +1,16 @@
+variable "prefix" {
+  type    = string
+  default = "sanglab"
+}
+variable "location" {
+  type    = string
+  default = "southeastasia"
+}
+variable "rg_name" {
+  type    = string
+  default = "sangndt1-devops-intern"
+}
+variable "vnet_cidr" {
+  type    = string
+  default = "10.10.0.0/16"
+}
